16841c7ad0a0f635b785ec2dc567f312b2261518
Galina <galinka-ver@mail.ru>
2023-02-04 23:57:17 +0500

    Сумма в промежутке

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine("Введите первое число ");
+int n = Convert.ToInt32(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе число ");
+int m = Convert.ToInt32(Console.ReadLine() ?? "");
+if (m==n) Console.WriteLine("Числа равны");
+else if (n>m)
+{
+    int k=m;
+    m=n;
+    n=k;
+} 
+Numbers(n,m);
+void Numbers(int n, int m)
+{
+    Console.Write($"Сумма = {SumNum(n - 1, m)} ");
+}
+int SumNum(int n, int m)
+{
+    int p = n;
+    if (n==m) return 0;
+    else
+    { 
+        n++;
+        p = n + SumNum(n, m);
+        return p;
+    }
+}
